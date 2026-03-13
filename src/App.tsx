88047dec4c0a9b8ca6d447e4bf1201c7a38d2d39
Vika <vikapetrova8@gmail.com>
2026-03-13 22:08:53 +0300

    feat: dynamic color, status handling, radius prop

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ export const App = () => {
     {
         const value = +e.target.value
         setValue(value)
-        if (value > 0 && value <= 99) {
-            setStatus('inProgress')
-        }
         if (value === 100) {
             setStatus('success')
+        } else {
+            setStatus('inProgress')
+
         }
     }
 
@@ -23,7 +23,7 @@ export const App = () => {
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
             <input type="number" min={0} max={100} value={value} onChange={onChangeHandler}/>
             <span>set progress</span>
-            <ProgressBar value={value} status={status} type={'dashboard'}/>
+            <ProgressBar value={value} status={status} type={'circle'}/>
         </div>
 
     )
--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -4,45 +4,78 @@ type Props = {
     value: number
     status: Status
     type?: 'circle' | 'dashboard'
+    radius?: number
 }
 
-export const ProgressBar = ({value, status, type = 'circle'}: Props) => {
+const progressColors = {
+    inProgress: '#209ffd',
+    success: '#12cc65',
+    error: '#fd4848',
+    warning: '#e5a13d',
+}
 
-    const radius = 75
+const baseStrokeColor = '#f4f5f8'
+
+const getColor = (value: number) => {
+    if (value < 30) return progressColors.error
+    if (value < 50) return progressColors.warning
+    if (value <= 99) return progressColors.inProgress
+    return progressColors.success
+}
+
+export const ProgressBar = ({value, status, type = 'circle', radius = 75}: Props) => {
+
+    const strokeWidth = 8
+    const size = radius * 2 + strokeWidth
+    const center = size / 2
+
     const fullLength = 2 * Math.PI * radius
-    const arcLength = type === 'circle' ? fullLength : (6 / 8 * fullLength)
+    const arcLength = type === 'circle' ? fullLength : (0.75 * fullLength)
     const offset = arcLength - (arcLength * value / 100)
 
-    const baseStrokeColor = '#f4f5f8'
-    const progressColors = {
-        inProgress: '#209ffd',
-        success: '#12cc65',
-        error: '#fd4848',
-        warning: '#e5a13d',
-    }
+
+    const color = getColor(value)
 
     const strokeDasharray = type === 'circle' ? fullLength : `${arcLength} ${fullLength}`
     const startPosition = type === 'circle' ? 'rotate(-90deg)' : 'rotate(-225deg)'
+
+    const renderContent = () => {
+        switch (status) {
+            case "inProgress":
+                return <p>{value}<span>%</span></p>
+            case 'error':
+                return <span style={{color: progressColors.error}}>х</span>
+            case 'warning':
+                return <span style={{
+                    color: '#fff',
+                    border: `1px solid ${progressColors.warning}`,
+                    backgroundColor: progressColors.warning,
+                    borderRadius: '50%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '16px',
+                    height: '16px'
+                }}>!</span>
+            case 'success':
+                return <span style={{color: progressColors.success}}>✓</span>
+        }
+    }
 
 
     return (
         <div style={{position: 'relative', marginTop: '40px'}}>
             <div style={{position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <svg width={160} height={160}>
-                    <circle cx="80" cy="80" r={radius} fill={'none'} stroke={baseStrokeColor} strokeWidth="8"
-                            strokeDasharray={strokeDasharray} strokeDashoffset={0} strokeLinecap={'round'}
-                            style={{
-                                transform: startPosition,
-                                transformOrigin: '80px 80px'
-                    }}>
+                <svg width={size} height={size} style={{transform: startPosition, transformOrigin: 'center'}}>
+                    <circle cx={center} cy={center} r={radius} fill={'none'} stroke={baseStrokeColor}
+                            strokeWidth={strokeWidth}
+                            strokeDasharray={strokeDasharray} strokeDashoffset={0} strokeLinecap={'round'}>
                     </circle>
-                    <circle cx="80" cy="80" r={radius} fill={'none'}
-                            stroke={progressColors[status]}
-                            strokeWidth="8" strokeDasharray={strokeDasharray} strokeDashoffset={offset}
+                    <circle cx={center} cy={center} r={radius} fill={'none'}
+                            stroke={color}
+                            strokeWidth={strokeWidth} strokeDasharray={strokeDasharray} strokeDashoffset={offset}
                             strokeLinecap={'round'}
                             style={{
-                                transform: startPosition,
-                                transformOrigin: '80px 80px',
                                 transition: 'stroke-dashoffset 0.5s, stroke 0.5s'
                             }}>
                     </circle>
@@ -58,7 +91,7 @@ export const ProgressBar = ({value, status, type = 'circle'}: Props) => {
                     width: '100%',
                     height: '100%'
                 }}>
-                    <p>{value}<span>%</span></p>
+                    {renderContent()}
                 </div>
             </div>
         </div>
